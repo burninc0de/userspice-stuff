42a8963728e4b3b41928b65c33c401edfc96729c
barrenc0de <44199273+burninc0de@users.noreply.github.com>
2024-03-23 12:09:26 +0200

    customfunc

--- usersc/includes/custom_functions.php
+++ usersc/includes/custom_functions.php
@@ -1,4 +1,26 @@
 <?php
+/*
+UserSpice 5
+An Open Source PHP User Management System
+by the UserSpice Team at http://UserSpice.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//Put your custom functions in this file and they will be automatically included.
+
+//bold("<br><br>custom helpers included");
 
 // Singleton class for Redis connection
 class RedisSingleton {
@@ -19,6 +41,7 @@ class RedisSingleton {
 }
 
 // Centralized function for Redis operations
+if (!function_exists('redisOperation')) {
 function redisOperation($key, $value = null, $expiration = null) {
     $redis = RedisSingleton::getInstance();
     if ($value !== null) {
@@ -32,6 +55,7 @@ function redisOperation($key, $value = null, $expiration = null) {
         // Get value from Redis
         return $redis->get($key);
     }
+}
 }
 
 ///
